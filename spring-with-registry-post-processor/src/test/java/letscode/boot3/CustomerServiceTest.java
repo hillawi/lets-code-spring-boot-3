3754b18bcdd73ac3d2bbceddf3bab2006bee6554
Ahmed Hillawi <abdeenahmed2002@hotmail.com>
2023-02-21 19:41:36 +0100

    Added spring with bean definition registry post processor

---
+++ spring-with-registry-post-processor/src/test/java/letscode/boot3/CustomerServiceTest.java
@@ -0,0 +1,16 @@
+package letscode.boot3;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.sql.DataSource;
+
+class CustomerServiceTest {
+
+    @Test
+    void all() throws Exception  {
+
+
+    }
+
+}
