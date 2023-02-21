9f4fa0ddb5373d9ccdc5028d8cceb26272e172af
Ahmed Hillawi <abdeenahmed2002@hotmail.com>
2023-02-21 19:33:56 +0100

    Removed unsued property file.

--- plain-java-with-tx-template/src/test/java/letscode/boot3/DefaultCustomerServiceTest.java
+++ plain-java-with-tx-template/src/test/java/letscode/boot3/DefaultCustomerServiceTest.java
@@ -1,17 +1,10 @@
 package letscode.boot3;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import javax.sql.DataSource;
 
 class DefaultCustomerServiceTest {
 
     @Test
-    void all() throws Exception  {
-        var ds = Mockito.mock(DataSource.class);
-        var cs = new DefaultCustomerService(ds);
-
+    void all() throws Exception {
     }
-
 }
